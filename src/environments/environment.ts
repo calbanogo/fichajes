7fce54b1b918566418ca8a0d97c52adc48a871da
Christian Albano <calbanogo@gmail.com>
2025-09-01 17:35:45 +0200

    feat: remove environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     authDomain: "fichajesapp-bb1d7.firebaseapp.com",
     projectId: "fichajesapp-bb1d7",
     storageBucket: "fichajesapp-bb1d7.firebasestorage.app",
-    messagingSenderId: "852527044815",
+    messagingSenderId: "852527044815", 
     appId: "1:852527044815:web:eaaf8877ea497074e5bfba"
   },
 };
